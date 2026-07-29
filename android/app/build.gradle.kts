6d1a751675cbaf273578146c8f2074d7487f4c33
Jongseop Park <qkrwhdtjq549@gmail.com>
2026-07-29 12:50:12 +0900

    build: 코어 라이브러리 디슈가링 활성화 (flutter_local_notifications 요구사항)

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -14,6 +14,8 @@ android {
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
+        // flutter_local_notifications(AAR)가 코어 라이브러리 디슈가링을 요구함.
+        isCoreLibraryDesugaringEnabled = true
     }
 
     kotlinOptions {
@@ -40,4 +42,8 @@ android {
 
 flutter {
     source = "../.."
+}
+
+dependencies {
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.4")
 }
